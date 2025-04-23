d8ede14a2bd36ea40e659ed1125aeff0f3ef6388
Ghoko <officialghoko@gmail.com>
2025-04-22 20:23:17 -0500

    Full FFMPEG conversion

--- Scripts/Crop.gd
+++ Scripts/Crop.gd
@@ -9,7 +9,7 @@ var exportpath : String
 var filename : String 
 var inputfile : String
 var outputfile : String
-var frames : Array = []
+var framerate : String
 
 func _ready():
 	Dialog.access = FileDialog.ACCESS_FILESYSTEM
@@ -24,37 +24,27 @@ func _input(event):
 				get_viewport().set_input_as_handled()
 
 
-func _process(delta: float) -> void:
-	if streamplayer.is_playing():
-		var tex = streamplayer.get_video_texture()
-		if tex:
-			var img = tex.get_image()
-			if img and not img.is_empty():
-				frames.append(img.duplicate())
-
-
 func _on_file_dialog_file_selected(path: String) -> void:
-	frames = []
 	inputfile = path
 	$FileLabel.text = inputfile
-	outputfile = path.get_basename() + ".ogv"
-	if !FileAccess.file_exists(outputfile):
-		push_error("No se pudo convertir el archivo.")
 
 func convert_gif_to_ogv(input_path: String, output_path: String):
 	var ffmpeg_path = get_ffmpeg_path()
+	var padding = get_padding()
 	var args = [
-		"-y",
+		"-r", "1",
 		"-i", input_path,
-		"-c:v", "libtheora",
-		"-q:v", "7",
-		output_path
+		"-r", framerate,
+		output_path,
 	]
-
 	var result = OS.execute(ffmpeg_path, args,[], false)
 	if result != 0:
 		push_error("Error ejecutando FFmpeg (código " + str(result) + ")")
 
+func get_padding():
+	var image_width = "iw"
+	var image_heigth = "ih"
+	
 func get_ffmpeg_path() -> String:
 	var os_name = OS.get_name()
 
@@ -67,43 +57,42 @@ func get_ffmpeg_path() -> String:
 			push_error("Sistema operativo no soportado")
 			return ""
 
+func _on_convert_button_pressed() -> void:
+	if !inputfile:
+		popup.title = "Error"
+		popup.dialog_text = "You haven't specified a file to convert"
+		popup.ok_button_text = "Close"
+		popup.popup_centered()
+	if !exportpath:
+		popup.title = "Error"
+		popup.dialog_text = "You haven't specified an output path"
+		popup.ok_button_text = "Close"
+		popup.popup_centered()
+	elif !filename:
+		popup.title = "Error"
+		popup.dialog_text = "You haven't specified a filename"
+		popup.ok_button_text = "Close"
+		popup.popup_centered()
+	elif !framerate:
+		popup.title = "Error"
+		popup.dialog_text = "You haven't specified a framerate"
+		popup.ok_button_text = "Close"
+		popup.popup_centered()
+	else:
+		outputfile = exportpath + "/" + filename + "-%d.png"
+		print(inputfile)
+		print(outputfile)
+		convert_gif_to_ogv(inputfile, outputfile)
+		
 func _on_button_pressed() -> void:
 	Dialog.popup_centered()
-
-
-
-func _on_button_2_pressed() -> void:
-	print(inputfile)
-	print(outputfile)
-	convert_gif_to_ogv(inputfile, outputfile)
-	if FileAccess.file_exists(outputfile):
-		streamplayer.stream = VideoStreamTheora.new()
-		streamplayer.stream = load(outputfile)
-		streamplayer.play()
-
-
 func _on_output_button_pressed() -> void:
 	Output.popup_centered()
-
-
-
 func _on_output_dialogue_dir_selected(dir: String) -> void:
 	exportpath = dir
 	$OutputLabel.text = dir
-
-
 func _on_filename_text_changed() -> void:
 	filename = textedit.text
-
-
-func _on_video_stream_player_finished() -> void:
-	for x in frames.size():
-		var texture = frames[x]
-		if exportpath and filename:
-			texture.save_png(exportpath + "/"+ filename + "-" + str(x)+ ".png")
-		else:
-			popup.title = "Error"
-			popup.dialog_text = "You haven't specified an output path or filename"
-			popup.ok_button_text = "Close"
-			popup.popup_centered()
+func _on_frameratebox_value_changed(value: float) -> void:
+	framerate = str(value)
 	
